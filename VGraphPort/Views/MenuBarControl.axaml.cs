1e5831b202165042ce1f381e7e842052aa011859
inglonias <JPNyden@gmail.com>
2026-09-04 15:13:07 -0400

    Renamed UserControl1.axaml to MenuBarControl.axaml

--- VGraphPort/Views/MenuBarControl.axaml.cs
+++ VGraphPort/Views/MenuBarControl.axaml.cs
@@ -4,9 +4,9 @@ using Avalonia.Markup.Xaml;
 
 namespace VGraphPort.Views
 {
-    public partial class UserControl1 : UserControl
+    public partial class MenuBarControl : UserControl
     {
-        public UserControl1()
+        public MenuBarControl()
         {
             InitializeComponent();
         }
