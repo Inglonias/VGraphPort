8d1acb9a9c6164828f972ada596acd4d3994546d
inglonias <JPNyden@gmail.com>
2026-09-06 16:12:24 -0400

    Got the background layer to render.

--- VGraphPort/Views/MainCanvas.cs
+++ VGraphPort/Views/MainCanvas.cs
@@ -1,6 +1,11 @@
+using System.IO;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using SkiaSharp;
+using VGraphPort.DataLayers;
+
 namespace VGraphPort.Views;
 
 public class MainCanvas : UserControl
@@ -9,9 +14,18 @@ public class MainCanvas : UserControl
 	{
 		base.Render(context);
 
-		context.DrawRectangle(
-			Brushes.Blue,
-			null,
-			new Rect(50, 50, 100, 100));
+		IDataLayer gridBackgroundLayer = new GridBackgroundLayer();
+		
+		//CAUTION: Code below was AI-generated. It converts an SKBitmap to something Avalonia can draw.
+		SKBitmap backgroundBitmap = gridBackgroundLayer.GenerateLayerBitmap();
+
+		using (var stream = new MemoryStream())
+		{
+			backgroundBitmap.Encode(stream, SKEncodedImageFormat.Png, 100);
+			stream.Position = 0;
+			var backgroundImage = new Bitmap(stream);
+    
+			context.DrawImage(backgroundImage, new Rect(0, 0, backgroundBitmap.Width, backgroundBitmap.Height));
+		}
 	}
 }
---
+++ VGraphPort/config/VgpFile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using VGraphPort.objects;
+
+namespace VGraphPort.config
+{
+    //Class used to serialize a canvas for saving and loading files.
+    internal class VgpFile
+    {
+        public int SquaresWide { get; set; }
+        public int SquaresTall { get; set; }
+        public int SquareSize { get; set; }
+        public int MarginX { get; set; }
+        public int MarginY { get; set; }
+        public string BackgroundImagePath { get; set; }
+
+        public List<LineSegment> Lines { get; set; }
+        public List<TextLabel> Labels { get; set; }
+    }
+}
