1cafe41136f4a099220aad0fc1da3a32d16797cd
Inglonias <2125926+Inglonias@users.noreply.github.com>
2026-09-09 10:55:33 -0400

    Fixed bugs and added a scrollviewer.

--- VGraphPort/Views/MainCanvas.cs
+++ VGraphPort/Views/MainCanvas.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
 using VGraphPort.DataLayers;
@@ -9,11 +10,20 @@ public class MainCanvas : Control
 	GridBackgroundLayer LGrid = new GridBackgroundLayer();
 	LayerDrawOperation DrawOp = new LayerDrawOperation();
 
+	public MainCanvas()
+	{
+		DrawOp.Layers.Add(LGrid);
+	}
+
 	public override void Render(DrawingContext context)
 	{
 		base.Render(context);
 		LGrid.GenerateLayerImage();
-		DrawOp.Layers.Add(LGrid);
 		context.Custom(DrawOp);
+	}
+
+	protected override Size MeasureOverride(Size availableSize)
+	{
+		return new Size(DrawOp.Bounds.Width, DrawOp.Bounds.Height);
 	}
 }
