f4ac98ec43a13f0832944b6ee6861772e57c36b4
Inglonias <2125926+Inglonias@users.noreply.github.com>
2026-09-09 15:27:42 -0400

    More menu bar work. Made buttons checkable and added code for selecting tools (buttons are not yet hooked up)

--- VGraphPort/Views/MenuBarControl.axaml.cs
+++ VGraphPort/Views/MenuBarControl.axaml.cs
@@ -1,6 +1,12 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
+using VGraphPort.Config;
+using VGraphPort.DataLayers;
+using VGraphPort.Objects;
 
 namespace VGraphPort.Views
 {
@@ -10,5 +16,36 @@ namespace VGraphPort.Views
         {
             InitializeComponent();
         }
-    }
+
+		private void ToolMenu_OnChecked(object sender, RoutedEventArgs e)
+		{
+			ToggleButton toolClicked = (ToggleButton)sender;
+			string targetTool = (string)toolClicked.Name;
+			SelectTool(targetTool);
+			InvalidateVisual();
+		}
+
+		private void SelectTool(string tool)
+		{
+			List<ToggleButton> toolMenuItems =
+			[
+				Line_Tool,
+				Tri_Tool,
+				Box_Tool,
+				Circle_Tool,
+				Boxy_Circle_Tool,
+				Ellipse_Tool,
+				Text_Tool,
+			];
+
+			foreach (ToggleButton m in toolMenuItems)
+			{
+				m.IsChecked = m.Name.Equals(tool);
+			}
+			LineLayer lineLayer = (LineLayer)PageData.Instance.GetDataLayer(PageData.LINE_LAYER);
+			TextLayer textLayer = (TextLayer)PageData.Instance.GetDataLayer(PageData.TEXT_LAYER);
+			lineLayer.SelectTool(tool);
+			textLayer.SelectTool(tool);
+		}
+	}
 }
