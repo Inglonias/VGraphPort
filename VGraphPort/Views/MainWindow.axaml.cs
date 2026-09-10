f00609b9d26328afc7160b6383956c2ce9c83bb3
Inglonias <2125926+Inglonias@users.noreply.github.com>
2026-09-10 08:51:50 -0400

    Plug in the other side of the events.

--- VGraphPort/Views/MainWindow.axaml.cs
+++ VGraphPort/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using VGraphPort.Config;
 
 namespace VGraphPort.Views;
 
@@ -7,5 +8,16 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-    }
+		PrimaryDrawingPanel.LineCreated += (_, _) =>
+		{
+			MainMenuBar.CheckEditButtonValidity();
+		};
+		PrimaryDrawingPanel.EyedropperUsed += (_, _) =>
+		{
+			PageData.Instance.IsEyedropperActive = false;
+			MainMenuBar.Eyedropper_Tool.IsChecked = false;
+			MainMenuBar.InvalidateVisual();
+			//MainMenuBar.ColorSwatch.InvalidateVisual();
+		};
+	}
 }
