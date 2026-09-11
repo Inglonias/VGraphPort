def7d350711e635903c60ada3fa91627b9ed8415
inglonias <JPNyden@gmail.com>
2026-09-11 19:47:11 -0400

    Added the Mirror Tool skeleton back.

---
+++ VGraphPort/Views/MirrorToolWindow.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace VGraphPort.Views;
+
+public partial class MirrorToolWindow : Window
+{
+    public MirrorToolWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void MirrorDirection_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        
+    }
+
+    private void MirrorToolWindow_OnOk(object sender, RoutedEventArgs e)
+    {
+        
+    }
+    
+    private void MirrorToolWindow_OnCancel(object sender, RoutedEventArgs e)
+    {
+        
+    }
+}
