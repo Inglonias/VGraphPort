8cb3f5f10d91f188560d9a9dfbe2d6f3c9cb851c
inglonias <JPNyden@gmail.com>
2026-09-11 19:31:39 -0400

    Added save, load, and import functionality

--- VGraphPort/Views/MenuBarControl.axaml.cs
+++ VGraphPort/Views/MenuBarControl.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
 using System.Collections.Generic;
+using Avalonia.Platform.Storage;
 using VGraphPort.Config;
 using VGraphPort.DataLayers;
 using VGraphPort.ViewModels;
@@ -69,24 +70,65 @@ namespace VGraphPort.Views
             OpenNewGridWindow(false);
         }
 
-        private void OpenButton_OnClick(object? sender, RoutedEventArgs e)
+        private async void OpenButton_OnClick(object? sender, RoutedEventArgs e)
         {
-            
+            var topLevel = TopLevel.GetTopLevel(this);
+            var file = await topLevel!.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open VGP File",
+                AllowMultiple = false,
+                FileTypeFilter = [Vgp]
+            });
+            if (file.Count > 0)
+            {
+                PageData.Instance.FileOpen(file[0].Path.ToString().Substring(7)); //Remove the preceding "file//")
+                ParentWindow.PrimaryDrawingPanel.InvalidateVisual();
+                ParentWindow.PrimaryDrawingPanel.InvalidateMeasure();
+            }
         }
 
         private void SaveButton_OnClick(object? sender, RoutedEventArgs e)
         {
-            
+            if (!string.IsNullOrEmpty(PageData.Instance.LastSavePath))
+            {
+                PageData.Instance.FileSave(PageData.Instance.LastSavePath);
+            }
+            else
+            {
+                SaveAsButton_OnClick(sender, e);
+            }
         }
         
-        private void SaveAsButton_OnClick(object? sender, RoutedEventArgs e)
+        private async void SaveAsButton_OnClick(object? sender, RoutedEventArgs e)
         {
-            
+            var topLevel = TopLevel.GetTopLevel(this);
+            var file = await topLevel!.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+            {
+                Title = "Save VGP File",
+                DefaultExtension = ".vgp",
+                SuggestedFileType = Vgp
+            });
+            if (file is not null)
+            {
+                PageData.Instance.FileOpen(file.Path.ToString().Substring(7)); //Remove the preceding "file//")
+            }
         }
         
-        private void ImportButton_OnClick(object? sender, RoutedEventArgs e)
+        private async void ImportButton_OnClick(object? sender, RoutedEventArgs e)
         {
-            
+            var topLevel = TopLevel.GetTopLevel(this);
+            var file = await topLevel!.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Import VGP File",
+                AllowMultiple = false,
+                FileTypeFilter = [Vgp]
+            });
+            if (file.Count > 0)
+            {
+                PageData.Instance.FileOpen(file[0].Path.ToString().Substring(7)); //Remove the preceding "file//")
+                ParentWindow.PrimaryDrawingPanel.InvalidateVisual();
+                ParentWindow.PrimaryDrawingPanel.InvalidateMeasure();
+            }
         }
         
         private void ExportButton_OnClick(object? sender, RoutedEventArgs e)
@@ -105,8 +147,25 @@ namespace VGraphPort.Views
             ngw.NewGridWindowComplete += (_, _) =>
             {
                 ParentWindow.PrimaryDrawingPanel.InvalidateVisual();
+                ParentWindow.PrimaryDrawingPanel.InvalidateMeasure();
             };
             ngw.Show();
         }
+        
+        private static FilePickerFileType Vgp { get; } = new FilePickerFileType("VGP File")
+        {
+            Patterns = (IReadOnlyList<string>) new string[1]
+            {
+                "*.vgp"
+            },
+            AppleUniformTypeIdentifiers = (IReadOnlyList<string>) new string[1]
+            {
+                "public.json"
+            },
+            MimeTypes = (IReadOnlyList<string>) new string[1]
+            {
+                "application/json"
+            }
+        };
     }
 }
