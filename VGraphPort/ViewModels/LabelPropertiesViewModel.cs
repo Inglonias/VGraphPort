c204338cea0a881bf50b6e3c39a0d45260ec98cc
inglonias <JPNyden@gmail.com>
2026-09-13 18:41:27 -0400

    LabelPropertiesWindow ported

---
+++ VGraphPort/ViewModels/LabelPropertiesViewModel.cs
@@ -0,0 +1,37 @@
+using Avalonia.Media;
+using Avalonia.Media.Fonts;
+using Avalonia.Skia;
+using CommunityToolkit.Mvvm.ComponentModel;
+using VGraphPort.Config;
+
+namespace VGraphPort.ViewModels;
+
+public partial class LabelPropertiesViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    public partial IFontCollection InstalledFonts { get; set; }
+
+    public Color ChosenColor
+    {
+        get { return _chosenColor; }
+        set { _chosenColor = SetChosenColor(value); }
+        
+    }
+    private Color _chosenColor;
+    
+    public LabelPropertiesViewModel()
+    {
+        InstalledFonts = FontManager.Current.SystemFonts;
+        _chosenColor = Color.FromArgb(PageData.Instance.CurrentLabelColor.Alpha,
+            PageData.Instance.CurrentLabelColor.Red, 
+            PageData.Instance.CurrentLabelColor.Green,
+            PageData.Instance.CurrentLabelColor.Blue);
+    }
+    
+    public Color SetChosenColor(Color newColor)
+    {
+        _chosenColor = newColor;
+        PageData.Instance.CurrentLabelColor = newColor.ToSKColor();
+        return _chosenColor;
+    }
+}
---
+++ VGraphPort/Views/LabelPropertiesWindow.axaml.cs
@@ -0,0 +1,24 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Media.Fonts;
+
+namespace VGraphPort.Views;
+
+public partial class LabelPropertiesWindow : Window
+{
+    public LabelPropertiesWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Okay_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    private void Cancel_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
