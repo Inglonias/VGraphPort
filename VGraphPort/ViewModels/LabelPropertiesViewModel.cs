d9140b987094d494c44948f9ae3ba8f257489fbc
Inglonias <2125926+Inglonias@users.noreply.github.com>
2026-09-15 14:37:09 -0400

    Laying out the label properties window.

--- VGraphPort/ViewModels/LabelPropertiesViewModel.cs
+++ VGraphPort/ViewModels/LabelPropertiesViewModel.cs
@@ -14,7 +14,23 @@ public partial class LabelPropertiesViewModel : ViewModelBase
     [ObservableProperty] public partial string LabelTextInUi { get; set; }
     [ObservableProperty] public partial int FontSizeInUi { get; set; }
     [ObservableProperty] public partial FontFamily FontInUi { get; set; }
+    [ObservableProperty] public partial AlignmentOption SelectedAlignmentInUi { get; set; }
     [ObservableProperty] public partial int AlignmentIntInUi { get; set; }
+
+    public readonly record struct AlignmentOption(string DisplayText, int Value);
+
+    public AlignmentOption[] AlignmentOptions { get; } =
+    {
+        new AlignmentOption("Top Left", 1),
+        new AlignmentOption("Top Center", 2),
+        new AlignmentOption("Top Right", 3),
+        new AlignmentOption("Center Left", 4),
+        new AlignmentOption("Center Center", 5),
+        new AlignmentOption("Center Right", 6),
+        new AlignmentOption("Bottom Left", 7),
+        new AlignmentOption("Bottom Center", 8),
+        new AlignmentOption("Bottom Right", 9),
+    };
 
     public Color ChosenColorInUi
     {
@@ -40,6 +56,7 @@ public partial class LabelPropertiesViewModel : ViewModelBase
             }
         }
         AlignmentIntInUi = TargetLabel.Alignment;
+        SelectedAlignmentInUi = AlignmentOptions[AlignmentIntInUi];
         _chosenColorInUi = Color.FromArgb(PageData.Instance.CurrentLabelColor.Alpha,
             PageData.Instance.CurrentLabelColor.Red,
             PageData.Instance.CurrentLabelColor.Green,
@@ -51,5 +68,10 @@ public partial class LabelPropertiesViewModel : ViewModelBase
         _chosenColorInUi = newColor;
         PageData.Instance.CurrentLabelColor = newColor.ToSKColor();
         return _chosenColorInUi;
+    }
+
+    public void FinishLabelEditing()
+    {
+
     }
 }
