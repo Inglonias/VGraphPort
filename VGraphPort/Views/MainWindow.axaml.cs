c6afcc1df6a561e2d6b190c633b55ba4e83fa181
Inglonias <2125926+Inglonias@users.noreply.github.com>
2026-09-15 11:30:33 -0400

    I missed another file!

--- VGraphPort/Views/MainWindow.axaml.cs
+++ VGraphPort/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Avalonia.Controls;
+using VGraphPort.Objects;
 using VGraphPort.ViewModels;
 
 namespace VGraphPort.Views;
@@ -13,11 +14,29 @@ public partial class MainWindow : Window
         MainMenuBar.ParentWindow = this;
     }
 
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is MainViewModel vm)
+        {
+            vm.EditTextLabelEvent += (_, targetLabel) => EditLabel(targetLabel);
+        }
+    }
     private void PrimaryDrawingPanel_OnRenderEvent(object? sender, EventArgs e)
     {
         if (DataContext is MainViewModel vm)
         {
             vm.SetWindowTitle();
         }
+    }
+
+    private void EditLabel(TextLabel target)
+    {
+        LabelPropertiesWindow lpw = new LabelPropertiesWindow
+        {
+            ParentWindow = this,
+            DataContext = new LabelPropertiesViewModel(target)
+        };
+        lpw.Show();
     }
 }
